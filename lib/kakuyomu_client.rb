95c6508000733bd5b7aa0d0857d97762c15e919c
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-11-03 11:15:12 +0900

    Initial commit

---
+++ lib/kakuyomu_client.rb
@@ -0,0 +1,6 @@
+require "kakuyomu_client/version"
+
+module KakuyomuClient
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kakuyomu_client/version.rb
@@ -0,0 +1,3 @@
+module KakuyomuClient
+  VERSION = "0.1.0"
+end
