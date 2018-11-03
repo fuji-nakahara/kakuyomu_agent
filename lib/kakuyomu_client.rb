67ea83efcde99b6b6e4da3979732a5f14c066e2c
Fuji Nakahara <fujinakahara2032@gmail.com>
2018-11-03 18:12:58 +0900

    Implement #create_episode and #delete_episode

--- lib/kakuyomu_client.rb
+++ lib/kakuyomu_client.rb
@@ -4,6 +4,7 @@ require_relative "kakuyomu_client/version"
 
 class KakuyomuClient
   Error            = Class.new(StandardError)
+  NotLoggedInError = Class.new(Error)
 
   BASE_URL = 'https://kakuyomu.jp'
 
@@ -32,5 +33,39 @@ class KakuyomuClient
     @logged_in = true
   rescue Selenium::WebDriver::Error::WebDriverError => e
     raise Error, 'Logging in failed: ', e.message
+  end
+
+  def create_episode(work_id:, title:, body:)
+    raise NotLoggedInError unless logged_in?
+
+    driver.navigate.to("#{base_url}/my/works/#{work_id}/episodes/new")
+    driver.find_element(name: 'title').send_keys(title)
+    driver.find_element(name: 'body').send_keys(body)
+    driver.find_element(id: 'reserveButton').click
+    driver.find_element(id: 'reservationControl-footer').find_element(tag_name: 'button').click
+
+    Selenium::WebDriver::Wait.new.until do
+      driver.find_element(id: 'page-my-works-episodes-published')
+    end
+
+    driver.current_url[%r{episodes/(\d+)}, 1]
+  rescue Selenium::WebDriver::Error::WebDriverError => e
+    raise Error, 'Creating episode failed: ', e.message
+  end
+
+  def delete_episode(work_id:, episode_id:)
+    raise NotLoggedInError unless logged_in?
+
+    driver.navigate.to("#{base_url}/my/works/#{work_id}/episodes/#{episode_id}")
+    driver.find_element(id: 'contentMainHeader-toolButton').click
+    driver.find_element(id: 'contentAsideHeader').find_element(xpath: '//*[contains(text(), "ツール")]').click
+    driver.find_element(id: 'deleteEpisode').find_element(tag_name: 'button').click
+    driver.switch_to.alert.accept
+
+    Selenium::WebDriver::Wait.new.until do
+      driver.find_element(id: 'modelessMessage')
+    end
+  rescue Selenium::WebDriver::Error::WebDriverError => e
+    raise Error, 'Deleting episode failed: ', e.message
   end
 end
--- spec/kakuyomu_client_spec.rb
+++ spec/kakuyomu_client_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe KakuyomuClient do
 
     it { is_expected.to be true }
   end
+
+  describe '#create_episode and #delete_episode' do
+    let(:work_id) { ENV.fetch('WORK_ID') }
+    let(:title) { '投稿テスト' }
+    let(:body) { Time.now.to_s }
+
+    before do
+      client.login!(email: ENV.fetch('KAKUYOMU_EMAIL'), password: ENV.fetch('KAKUYOMU_PASSWORD'))
+    end
+
+    it 'creates and deletes episode without errors' do
+      expect do
+        episode_id = client.create_episode(work_id: work_id, title: title, body: body)
+        client.delete_episode(work_id: work_id, episode_id: episode_id)
+      end.not_to raise_error
+    end
+  end
 end
